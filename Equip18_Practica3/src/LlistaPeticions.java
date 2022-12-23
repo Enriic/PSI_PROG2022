3d81cb5e4850d131407388837b636f4a0ce4988a
ramzibelounis <belounis.moh@gmail.com>
2022-12-23 13:30:46 +0100

    Update LlistaPeticions.java

--- Equip18_Practica3/src/LlistaPeticions.java
+++ Equip18_Practica3/src/LlistaPeticions.java
@@ -1,20 +1,55 @@
+import java.util.Scanner;
 
 public class LlistaPeticions {
 
-	private Peticio[] llista;
+	private Peticion[] llista;
 	private int numElem;
 	
 	
 	public LlistaPeticions(int dim) {
-		llista = new Peticio[dim];
+		llista = new Peticion[];
 		numElem = 0;
 	}
 	
-	public void afegirPet(Peticio pet) {
+	public void afegirPet(Peticion pet) {
 		if (numElem < llista.length) {
 			llista[numElem] = pet;
 			numElem++;
 		}
+	}
+
+
+
+
+	public void acceptaorefusa(int estat){
+
+		System.out.println("Quina peticio vols acceptar o refusar?");
+
+		Scanner LlegirPet =new Scanner(System.in);
+
+		Peticion =LlegirPet.nextLine();
+
+
+		System.out.println("Vols acceptar o refusar?");
+
+		Scanner estatLlegir = new Scanner(System.in);
+		estat = estatLlegir.nextInt();
+		
+		if(estat==1){
+
+			getUsuariDemana().setValoracioOfert(valoracioDemanat);
+
+			getUsuariRep().setValoracioDemanat(valoracioOfert);
+
+
+		}
+
+		if(estat == 0){
+
+			System.out.println("Pendent");
+
+		}
+
 	}
 	
 	public void esborrarPet(String codi) {
@@ -33,5 +68,5 @@ public class LlistaPeticions {
 		// ...si Estat==2...
 	}
 	
-	
+
 }
