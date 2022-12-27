51e8a7bec00ce593b14920f604e51cd7a0be6b1a
ArgiURV <121185638+ArgiURV@users.noreply.github.com>
2022-12-27 19:10:05 +0100

    Peticio arreglado

--- Equip18_Practica3/src/Peticio.java
+++ Equip18_Practica3/src/Peticio.java
@@ -1,73 +1,59 @@
 public class Peticio {
 	private String codi;
-	private Usuari usuariDemana;
-	private Usuari usuariRep;
-	private String codiProducteDemanat;
-	private String codiProducteOfert;
-	private int valoracioDemanat;
-	private int valoracioOfert;
-	private int Estat;
+	private Usuari usuariA; 		// Usuario que HACE la petición
+	private Usuari usuariB; 		// Usuario que RECIBE la petición
+	private String codiProducteA;
+	private String codiProducteB;
+	private int valoracioA;
+	private int valoracioB;
+	private int estat;
 	
-	public Peticio(String codi, Usuari usuariDemana, Usuari usuariRep, String codiProducteDemanat, String codiProducteOfert) {
+	public Peticio(String codi, Usuari usuariA, Usuari usuariB, String codiProducteA, String codiProducteB) {
 		this.codi = codi;
-		this.usuariDemana = usuariDemana;
-		this.usuariRep = usuariRep;
-		this.codiProducteDemanat = codiProducteDemanat;
-		this.codiProducteOfert = codiProducteOfert;
-		this.Estat = 0;
-		
-	}
-
-	public Peticio(String codi, Usuari usuariDemana, Usuari usuariRep, String codiProducteDemanat, String codiProducteOfert, int val1, int val2) {
-		this.codi = codi;
-		this.usuariDemana = usuariDemana;
-		this.usuariRep = usuariRep;
-		this.codiProducteDemanat = codiProducteDemanat;
-		this.codiProducteOfert = codiProducteOfert;
-		this.valoracioDemanat=val1;
-		this.valoracioOfert=val2;
-		this.Estat = 0;
+		this.usuariA = usuariA;
+		this.usuariB = usuariB;
+		this.codiProducteA = codiProducteA;
+		this.codiProducteB = codiProducteB;
+		this.estat = 0;
 		
 	}
 	
 	public int getEstat() {
-		return Estat;
+		return estat;
 	}
-
-	public void setEstat(int estat) {
-		Estat = estat;
-	}
-	
 
 	public String getCodi() {
 		return codi;
 	}
 
-	public Usuari getUsuariDemana() {
-		return usuariDemana;
+	public Usuari getUsuariA() {
+		return usuariA;
 	}
 
-	public Usuari getUsuariRep() {
-		return usuariRep;
+	public Usuari getUsuariB() {
+		return usuariB;
 	}
 
-	public String getCodiProducteDemanat() {
-		return codiProducteDemanat;
+	public String getCodiProducteA() {
+		return codiProducteA;
 	}
 
-	public String getCodiProducteOfert() {
-		return codiProducteOfert;
+	public String getCodiProducteB() {
+		return codiProducteB;
 	}
 
-    public void setValoracioDemanat(int val1) {
-        usuariDemana.setValoracio(((usuariDemana.getValoracio()*usuariDemana.getIntercanvis())+val1)/(usuariDemana.getIntercanvis()+1));
-        usuariDemana.afegirIntercanvi();
-    }
-
-    public void setValoracioOfert(int val2) {
-        usuariRep.setValoracio(((usuariRep.getValoracio()*usuariRep.getIntercanvis())+val2)/(usuariRep.getIntercanvis()+1));
-        usuariRep.afegirIntercanvi();
-    }
+	public void AcceptarPet(int valA, int valB) {
+		estat = 1;
+		usuariA.setValoracio(((usuariA.getValoracio()*usuariA.getIntercanvis())+valA)/(usuariA.getIntercanvis()+1));
+		usuariA.afegirIntercanvi();
+		usuariB.setValoracio(((usuariB.getValoracio()*usuariB.getIntercanvis())+valB)/(usuariB.getIntercanvis()+1));
+		usuariB.afegirIntercanvi();
+		
+	}
+	
+	public void RefusarPet() {
+		estat = 2;
+	}
 
 }
 
